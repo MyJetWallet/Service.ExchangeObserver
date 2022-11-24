1861dc71ace4df29e17154cc6f99cff151516161
Pavlo Pylypenko <pavel.p@smplt.net>
2022-11-24 12:39:32 +0200

    migration

---
+++ src/Service.ExchangeObserver.Postgres/Migrations/20221124103808_EnabledFlag.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.ExchangeObserver.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnabledFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                schema: "exchangeobserver",
+                table: "assets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                schema: "exchangeobserver",
+                table: "assets");
+        }
+    }
+}
